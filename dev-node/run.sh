48765fd5c9957b66d9be3709cc20e745c3fce683
Hao <hao@sporule.com>
2020-05-25 15:21:02 +0000

    updated symbol link

--- dev-node/run.sh
+++ dev-node/run.sh
@@ -7,9 +7,10 @@ echo "export HADOOP_HOME=$HADOOP_HOME" >> "/root/.profile"
 echo "export HADOOP_CONF_DIR=$HADOOP_CONF_DIR" >> "/root/.profile"
 echo "export HIVE_HOME=$HIVE_HOME" >> "/root/.profile"
 echo "export HIVE_VERSION=$HIVE_VERSION" >> "/root/.profile"
+echo "export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH" >> "/root/.profile"
 
-# copy hive config to spark
-cp /opt/hive/conf/hive-site.xml /spark/conf/hive-site.xml
+# Link hive config to spark conf
+ln -s /opt/hive/conf/hive-site.xml /spark/conf/hive-site.xml
 
 # Run Airflow if it is enabled
 if [ "$AIRFLOW" = "1" ]; then airflow initdb; (airflow webserver -p 8080 &) ;(airflow scheduler &); fi
