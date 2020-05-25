f4668035a80d7714232b25d4542dd0f3fb559a0f
Hao <hao@sporule.com>
2020-05-25 19:51:40 +0000

    updated confi

--- dev-node/run.sh
+++ dev-node/run.sh
@@ -7,7 +7,9 @@ echo "export HADOOP_HOME=$HADOOP_HOME" >> "/root/.profile"
 echo "export HADOOP_CONF_DIR=$HADOOP_CONF_DIR" >> "/root/.profile"
 echo "export HIVE_HOME=$HIVE_HOME" >> "/root/.profile"
 echo "export HIVE_VERSION=$HIVE_VERSION" >> "/root/.profile"
+echo "export SPARK_HOME=$SPARK_HOME" >> "/root/.profile"
 echo "export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native:$LD_LIBRARY_PATH" >> "/root/.profile"
+
 
 # Link hive config to spark conf
 ln -s /opt/hive/conf/hive-site.xml /spark/conf/hive-site.xml
